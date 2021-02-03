6fe2ae7c95d06e23f6774159a36b7be779cb91f4
prathambongale <prathambongale@hotmail.com>
2021-02-03 14:52:52 +0200

    First Commit

---
+++ src/main/java/com/springboot/camel/microservice/two/camelmicroservicereceiver/CamelMicroserviceReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.camel.microservice.two.camelmicroservicereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/camel/microservice/two/camelmicroservicereceiver/CamelMicroserviceReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.camel.microservice.two.camelmicroservicereceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
